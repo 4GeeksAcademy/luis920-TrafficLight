6b25c7298542d5a1010b4c8e8e1e3a0483923269
Luis Galvan <67127384+luis920@users.noreply.github.com>
2024-10-30 00:44:20 +0000

    commit final

--- src/js/component/Semaforo.jsx
+++ src/js/component/Semaforo.jsx
@@ -1,29 +1,36 @@
 import React, { useState } from "react";
 
-function Semaforo(){
-    const[color,setColor]=useState("red")
+function Semaforo() {
+    const [color, setColor] = useState("red");
+    const colores = ["red", "yellow", "green"];
 
-    
+    const cambiarColor = () => {
+        const siguienteColor = colores[(colores.indexOf(color) + 1) % colores.length];
+        setColor(siguienteColor);
+    };
 
-    return(
-        <div className="container">
+   
 
-            <div 
-            onClick= {() => setColor(red) } 
-            className=" enfoque red">
-
-            </div>
-            <div 
-            onClick= {() => setColor(yellow) } 
-            className=" enfoque yellow">
-
-            </div>
+    return (
+        <>
+        <div className="container" >
             <div
-            onClick= {() => setColor(green) } 
-             className=" enfoque green"></div>
-
+                onClick={() => setColor("red")}
+                className={`light red ${color === "red" ? "light-red" : ""}`}
+            ></div>
+            <div
+                onClick={() => setColor("yellow")}
+                className={`light yellow ${color === "yellow" ? "light-yellow" : ""}`}
+            ></div>
+            <div
+                onClick={() => setColor("green")}
+                className={`light green ${color === "green" ? "light-green" : ""}`}
+            ></div>
         </div>
-    )
+        <button onClick={cambiarColor}>Cambiar color</button>
+        </>
+        
+    );
 }
 
-export default Semaforo
+export default Semaforo;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Semaforo from "./Semaforo.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+
 
 //create your first component
 const Home = () => {
